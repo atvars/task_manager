d53f4bb71f55f359f38860ca85ecee54c5fd52ac
atvars <avelavs@gmail.com>
2021-01-23 18:33:43 +0000

    adedd colapse task setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,4 @@
-/*
-    jQuery for MaterializeCSS initialization
-*/
-
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
 });
-
-
-
-
-
-/*
-    vanilla JavaScript for MaterializeCSS initialization
-*/
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     let sidenavs = document.querySelectorAll(".sidenav");
-//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-// });
